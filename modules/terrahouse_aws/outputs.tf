6c03737af57c8d34e7e3610d334c3a212ba28610
Tripatjeet Singh <txs030@umb.com>
2023-09-28 16:57:38 +0000

    #31 invalidate cloudfront stack when content version changes

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -4,3 +4,6 @@ output "bucket_name" {
 output "static_website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
+output "cloudfront_distribution_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,6 @@ output "bucket_name" {
 output "static_website_endpoint" {
   value = module.terrahouse_aws.static_website_endpoint
 }
+output "cloudfront_distribution_domain_name" {
+  value = module.terrahouse_aws.cloudfront_distribution_domain_name
+}
