5974ed724868a03bde50d4ab56a09afea6a39593
Tripatjeet Singh <txs030@umb.com>
2023-09-30 17:38:39 +0000

    fixing issues

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,6 +1,5 @@
 output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
-  sensitive = true
 }
 output "static_website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_config.website_endpoint
