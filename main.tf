65f0e43eabf21eb924ce7d812896c39e5e781b2e
Tripatjeet Singh <110929429+tripatjeetsingh@users.noreply.github.com>
2023-10-04 00:07:41 -0500

    39 terratowns provider (#41)
    * #39 work in progress for setting up a skeleton for creating a custom terraform provider
    
    * #39 implement terraform provider block in terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
   cloud {
     organization = "ACG-Terraform-Demos-Orgs"
     workspaces {
@@ -6,12 +12,17 @@ terraform {
     }
   }
 }
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  UserUuid            = var.UserUuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version     = var.content_version
-  assets_filepath     = var.assets_filepath
+provider "terratowns" {
+  endpoint = "http://localhost:4567"
+  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
+# module "terrahouse_aws" {
+#   source              = "./modules/terrahouse_aws"
+#   UserUuid            = var.UserUuid
+#   bucket_name         = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   content_version     = var.content_version
+#   assets_filepath     = var.assets_filepath
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "bucket_name" {
-  value = module.terrahouse_aws.bucket_name
-}
-output "static_website_endpoint" {
-  value = module.terrahouse_aws.static_website_endpoint
-}
-output "cloudfront_distribution_domain_name" {
-  value = module.terrahouse_aws.cloudfront_distribution_domain_name
-}
+# output "bucket_name" {
+#   value = module.terrahouse_aws.bucket_name
+# }
+# output "static_website_endpoint" {
+#   value = module.terrahouse_aws.static_website_endpoint
+# }
+# output "cloudfront_distribution_domain_name" {
+#   value = module.terrahouse_aws.cloudfront_distribution_domain_name
+# }
